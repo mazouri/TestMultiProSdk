132c716bce12bfe9d8cb61a8ca4f197ec130a100
wangdongdong <wangdd_bj@163.com>
2019-05-08 16:31:00 +0800

    Initial commit

---
+++ sdkLibOne/src/main/java/com/mazouri/sdk_lib_one/SdkLibOneUtil.java
@@ -0,0 +1,19 @@
+package com.mazouri.sdk_lib_one;
+
+/**
+ * @author 王冬冬
+ * @filename: SdkLibOneUtil
+ * @describe: TODO
+ * @date on 2019/5/8 10:58
+ * @owner: 王冬冬
+ * @second owner:
+ * @email: wangdd_bj@163.com
+ * @csdn: https://blog.csdn.net/dongdong230
+ * @github: https://github.com/mazouri
+ */
+public class SdkLibOneUtil {
+
+    public static String get() {
+        return "This string is from sdk lib one";
+    }
+}
---
+++ sdkLibTwo/src/main/java/com/mazouri/sdklibtwo/SdkLibTwoUtil.java
@@ -0,0 +1,19 @@
+package com.mazouri.sdklibtwo;
+
+/**
+ * @author 王冬冬
+ * @filename: SdkLibOneUtil
+ * @describe: TODO
+ * @date on 2019/5/8 10:58
+ * @owner: 王冬冬
+ * @second owner:
+ * @email: wangdd_bj@163.com
+ * @csdn: https://blog.csdn.net/dongdong230
+ * @github: https://github.com/mazouri
+ */
+public class SdkLibTwoUtil {
+
+    public static String get() {
+        return "This string is from sdk lib two";
+    }
+}
